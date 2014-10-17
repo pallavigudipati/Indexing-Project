8d0a03a4a12ad9a0d88b0ff7ecd3552f2e6da30a
pallavigudipati <pallavigudipati@gmail.com>
2014-10-16 20:14:47 -0700

    Test

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,18 @@
+import java.io.IOException;
+
+import it.unimi.dsi.logging.ProgressLogger;
+import it.unimi.dsi.webgraph.ImmutableGraph;
+
 
 public class Parser {
 
+    static public void main(String arg[]) {
+        ProgressLogger pl = new ProgressLogger();
+        try {
+            ImmutableGraph graph = ImmutableGraph.loadMapped("datasets/uk-2006-08/uk-2006-08", pl);
+                    //ImmutableGraph.load("datasets/uk-2006-08/uk-2006-08", pl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

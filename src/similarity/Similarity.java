c0d57509b8b4f79964b40399ea0d97dc7d310da1
Aritra Ghosh <aritra@aritrag.(none)>
2014-10-17 17:46:47 +0530

    VertexEdgeOverlap done

--- src/similarity/Similarity.java
+++ src/similarity/Similarity.java
@@ -12,47 +12,36 @@ import java.util.Set;
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
 
+public class Similarity {
 
-public  class Similarity
-{
+	private Similarity() {
+	}
 
- 
-  private Similarity()
-  {
-  } 
+	/***
+	 ** The starting point for the demo.
+	 ** 
+	 ** @param args
+	 *            ignored.
+	 */
+	public static void main(String[] args) {
+		Similarity h = new Similarity();
 
-  
-  /***
-   ** The starting point for the demo.
-   **
-   ** @param args ignored.
-   */
-  public static void main(String [] args)
-  {
-  	Similarity h = new Similarity();
-  	
-  	DirectedGraph<String, DefaultEdge> g1 = h.readGraph("data/web-Stanford.txt");
-  	DirectedGraph<String, DefaultEdge> g2 = h.readGraph("data/web-Stanford.txt");
-  	g2.removeVertex("1");
-  	Set<String> v=g1.vertexSet();
-    System.out.println("Number of vertices "+ v.size());        
+		DirectedGraph<String, DefaultEdge> g1 = h
+				.readGraph("data/web-Stanford.txt");
+		DirectedGraph<String, DefaultEdge> g2 = h
+				.readGraph("data/web-Stanford.txt");
+		g2.removeVertex("1");
+		Set<String> v = g1.vertexSet();
+		System.out.println("Vertex Edge Overlap between the Graphs is  "
+				+ h.vertexEdgeOverlap(g1, g2));
 
-    System.out.println("Vertex Edge Overlap between the Graphs is  "+ h.vertexEdgeOverlap(g1, g2));
+	}
 
-      
-  }
+	public DirectedGraph<String, DefaultEdge> readGraph(String filename) {
+		DirectedGraph<String, DefaultEdge> g = new DefaultDirectedGraph<String, DefaultEdge>(
+				DefaultEdge.class);
+		BufferedReader br = null;
 
-  
- 
-  private  DirectedGraph<String, DefaultEdge> readGraph(String filename)
-  {
-  	DirectedGraph<String, DefaultEdge> g =
-              new DefaultDirectedGraph<String, DefaultEdge>
-              (DefaultEdge.class);
-  	BufferedReader br = null;
-  	
-     
-  	 
 		try {
 
 			String sCurrentLine;
@@ -61,67 +50,66 @@ public  class Similarity
 
 			while ((sCurrentLine = br.readLine()) != null) {
 				String[] edge = sCurrentLine.split("\t");
-				if(!g.containsVertex(edge[0])){
+				if (!g.containsVertex(edge[0])) {
 					g.addVertex(edge[0]);
 				}
-				if(!g.containsVertex(edge[1])){
+				if (!g.containsVertex(edge[1])) {
 					g.addVertex(edge[1]);
 				}
-				g.addEdge(edge[0],edge[1]);
-					
-			
-				
+				g.addEdge(edge[0], edge[1]);
+
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (br != null)br.close();
+				if (br != null)
+					br.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 		}
 
-	
-  return g;
-  }
-  
-  public double  vertexEdgeOverlap(DirectedGraph<String, DefaultEdge> g1, DirectedGraph<String, DefaultEdge> g2){
-	  
-	  int n1 = g1.vertexSet().size();
-	  int n2 = g2.vertexSet().size();
-	  long e1 = g1.edgeSet().size();
-	  long e2 = g2.edgeSet().size();
-	  long commonEdges =0;
-	  int commonVertices = 0;
-	  double vertexEdgeOverlap=0.0;
-	
-	  Set<String>  nodes  = g1.vertexSet();
-	  for (String s: nodes){
-		  if(g2.containsVertex(s))
-			  commonVertices++;
-	  }
-	  
-	  
-	 
-	  Set<DefaultEdge> edgeset1 =g1.edgeSet();
-	  for(DefaultEdge e : edgeset1){
-		  System.out.println(e.toString());
-		  if(g2.containsEdge(e)){
-			  commonEdges++;
-		  }
-	  }
-	  
-	  
-	  
-	   
-	   vertexEdgeOverlap = 2*(double) (commonVertices + commonEdges)/(n1+n2+ e1 + e2);
-	   System.out.println("N1 = "+ n1 + " N2 "+n2 +" E1 "+ e1 + " E2 "+ e2 + " Common Edges "+ commonEdges + " Common vertices "+ commonVertices +" Answer " + vertexEdgeOverlap);
-	   
-	  return vertexEdgeOverlap;
-	  
-  }
-  
-  
+		return g;
+	}
+
+	public double vertexEdgeOverlap(DirectedGraph<String, DefaultEdge> g1,
+			DirectedGraph<String, DefaultEdge> g2) {
+
+		int n1 = g1.vertexSet().size();
+		int n2 = g2.vertexSet().size();
+		long e1 = g1.edgeSet().size();
+		long e2 = g2.edgeSet().size();
+		int commonEdges = 0;
+		int commonVertices = 0;
+		double vertexEdgeOverlap = 0.0;
+
+		Set<String> nodes = g1.vertexSet();
+		for (String s : nodes) {
+			if (g2.containsVertex(s))
+				commonVertices++;
+		}
+
+		Set<DefaultEdge> edgeset1 = g1.edgeSet();
+
+		for (DefaultEdge ed1 : edgeset1) {
+			String v1 = g1.getEdgeSource(ed1);
+			String v2 = g1.getEdgeTarget(ed1);
+
+			if (g2.containsEdge(v1, v2))
+				commonEdges++;
+
+		}
+
+		vertexEdgeOverlap = 2 * (double) (commonVertices + commonEdges)
+				/ (n1 + n2 + e1 + e2);
+		System.out.println("N1 = " + n1 + " N2 " + n2 + " E1 " + e1 + " E2 "
+				+ e2 + " Common Edges " + commonEdges + " Common vertices "
+				+ commonVertices + " Answer " + vertexEdgeOverlap);
+
+		return vertexEdgeOverlap;
+
+	}
+
 }
